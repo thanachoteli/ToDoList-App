ed62c1691436d2918a221895ffa3726f9d8bf822
thanachoteli <thanachoteli.2006@gmail.com>
2025-12-17 00:09:22 +0700

    Add new and index ejs and add routes

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const ejsMate = require("ejs-mate");
+
+app.engine("ejs", ejsMate);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-    res.render("home");
+	res.render("home");
 });
 
 app.get("/todolist", (req, res) => {
-    res.render("ntodolist/show");
+	res.render("todolist/index");
+});
+
+app.get("/todolist/new", (req, res) => {
+	res.render("todolist/new");
 });
 
 app.listen(4000, () => {
-    console.log("Listening on PORT 4000");
+	console.log("Listening on PORT 4000");
 });
